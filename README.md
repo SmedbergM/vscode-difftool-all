62caa7b6336366ae6d6077b276d81b773fe5c3d6
Matthew Smedberg <matthew.smedberg@gmail.com>
2021-10-10 22:33:18 -0600

    vscode_difftool_all :: initial commit

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # vscode-difftool-all
 Open all diffs between two commits in a single VSCode instance
+
+## Requirements
+This script requires only Python standard library modules. It invokes `git`
+and `code`.
+
+I wrote it for use on Posix systems; while it has not been tested on Windows,
+it should be portable. If you encounter a bug, feel free to raise an issue
+containing a minimal working example.
+
+## Basic usage
+
+Assuming that the current working diretory is under Git,
+```sh
+vscode_difftool_all.py <commit-ish> <commit-ish>
+```
+will find all the paths which differ between the two commits, and open the
+corresponding diffs in a single instance of Visual Studio Code.
+
+(A `<commit-ish>` is anything that `git rev-parse` can resolve: a full commit
+hash, a shortened hash, a branch name, a tag, etc.)
+
+Both versions will be written to temporary files; because the script does not
+wait for VSCode to close, it is the user's responsibility to clean up any
+temporary files and directories. The script will print the path of the
+generated temporary directory to standard out.
+
+If the second `<commit-ish>` is omitted, the current working tree will be
+used. In this case, only the left version will be written to temporary files;
+the right version will be opened from the working tree, so that edits will
+persist.
+
+## Recommended installation
+It is recommended that you add a symbolic link from somewhere on your `PATH`
+to the `vscode_difftool_all.py` script; for example, if `~/bin` is on your
+`PATH`, then running
+```sh
+cd ~/bin
+ln -s /path/to/vscode_difftool_all.py git-difftool-all
+```
+will enable invocations like
+```sh
+git difftool-all my-branch main
+```
+in any Git repository.
---
+++ vscode_difftool_all.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+
+import argparse
+import os
+import os.path
+import subprocess
+import tempfile
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("left", type=rev_parse)
+    parser.add_argument("right", nargs="?", type=rev_parse)
+    return parser.parse_args()
+
+def rev_parse(rev):
+    cmd = ["git", "rev-parse", "--short", rev]
+    p = subprocess.run(cmd, check=True, text=True, capture_output=True)
+    return p.stdout.strip()
+
+def list_paths(args):
+    cmd = ["git", "diff", "--name-only"] + [args.left, args.right]
+    cmd = list(filter(bool, cmd))
+    with subprocess.Popen(cmd, text=True, stdout=subprocess.PIPE) as p:
+        for line in p.stdout:
+            s = line.strip()
+            if s:
+                yield s
+
+    if p.returncode:
+        # The context manager waits until the process is complete, so
+        # returncode should not be None
+        msg = "{} returned status {}".format(" ".join(cmd), p.returncode)
+        raise subprocess.SubprocessError(msg)
+
+def main(args):
+    tmp = tempfile.mkdtemp(prefix="difftool-")
+    print(f"To clean up workspace, run\n    rm -rf {tmp}")
+    # It would be preferable to open up the temp directory itself as the
+    # workspace of the VSCode instance; however, that produces the Big
+    # Annoying "Do you trust the authors of the files in this folder?" modal
+    # unless the user has trusted /tmp, which is a bad idea.
+
+    def difftool(path, reuse_window):
+        (relative_dir, basename) = os.path.split(path)
+        absolute_dir = os.path.join(tmp, relative_dir)
+        os.makedirs(absolute_dir, exist_ok=True)
+        left_path = os.path.join(absolute_dir, f"{args.left}.{basename}")
+        with open(left_path, "w") as left:
+            cmd = ["git", "show", f"{args.left}:{path}"]
+            subprocess.run(cmd, check=False, stdout=left, stderr=subprocess.DEVNULL)
+        if args.right:
+            right_path = os.path.join(absolute_dir, f"{args.right}.{basename}")
+            with open(right_path, "w") as right:
+                cmd = ["git", "show", f"{args.right}:{path}"]
+                subprocess.run(cmd, check=True, stdout=right)
+        elif os.path.exists(path):
+            # Then we are diffing implicitly against HEAD, and we want to edit the working tree
+            right_path = path
+        else:
+            right_path = os.devnull
+        cmd = [
+            "code", "--diff",
+            "-r" if reuse_window else "-n",
+            left_path, right_path
+        ]
+        subprocess.run(cmd, check=True)
+
+    for (idx, path) in enumerate(list_paths(args)):
+        difftool(path, idx)
+
+
+if "__main__" == __name__:
+    args = parse_args()
+    main(args)
